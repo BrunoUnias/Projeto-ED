f0a5a331c01b1797da62756f025d13340e09fe38
BrunoUnias <37314205+BrunoUnias@users.noreply.github.com>
2019-11-13 19:13:01 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define TAM 20
+#define TAM 5
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 typedef struct Aluno{
 	char nome[20];
 	int matricula[6];
+	int *p;
 	
 	
 }Aluno;
 
 void receberAluno(Aluno aluno[]){
 	int i;
+	
 	for(i = 0; i<=TAM;i++){
 		system("cls");
 		printf("nome:");
-		scanf("%s",&aluno[i].nome);
+		scanf("%s",aluno[i].nome);
 		printf("matricula:");
-		scanf("%d",&aluno[i].matricula);
+		scanf("%d",aluno[i].matricula);
+		selectionSort(aluno[i].matricula,TAM);
 		
 	}
 	
@@ -27,10 +30,41 @@ void mostrar(Aluno aluno[]){
 	int i;
 	for(i=0;i<=TAM;i++){
 		printf("Nome: %s",aluno[i].nome);
-		printf("Matricula: %s",aluno[i].matricula);
+		printf("\n");
+		printf("Matricula: %d",aluno[i].matricula);
+		
 		
 	}
 	
+	
+}
+
+void swap(int *px,int *y){
+	int temp = *px;
+	*px = *y;
+	*y = temp;
+}
+
+int selectionSort(int arr[],int n){
+	int i,j, min_idx;
+	for(i=0;i<=n-1;i++){
+		min_idx = i;
+		for(j= i+1;j<n;j++){
+			if(arr[j] < arr[min_idx]){
+				min_idx = j;
+				
+				swap(&arr[min_idx],&arr[i]);
+			}
+		}
+	}
+}
+
+void printArray(int arr[],int size){
+	int i;
+	for(i=0;i<size;i++){
+		printf("%d",arr[i]);
+		printf("\n");
+	}
 }
 
 int main(int argc, char *argv[]) {
